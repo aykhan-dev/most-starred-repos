4347f59722b5b7b2910f8e75a9826a80fdd680dd
aykhan-dev <ayxan.ev@gmail.com>
2023-01-12 20:44:41 +0400

    added Hilt DI;

---
+++ data/src/main/java/ev/aykhn/data/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package ev.aykhn.data.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule
